7360d27c675bec11e0f58cd553902aaed4a597f6
dickwyn <dickwyn@outlook.com>
2019-11-03 10:32:10 -0700

    (feat) add toast for international night tickets

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Hero from './components/Hero';
 import Content from './components/Content';
 import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <Hero />
-      <Content />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    toast.info(
+      <p>
+        <span role="img" aria-label="world-map">
+          🗺️
+        </span>{' '}
+        International Night 2019 is happening on November 15, Get your free
+        tickets <a href="https://signup.asucis.com">here</a>
+        <br />
+        <br />
+        #ASUINTLNIGHT
+        <br />
+        #asuglobal
+      </p>,
+      {
+        position: 'bottom-center',
+        autoClose: 30000,
+        hideProgressBar: true,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true
+      }
+    );
+  }
+  render() {
+    return (
+      <div className="App">
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnVisibilityChange
+          draggable
+          pauseOnHover
+        />
+        <Hero />
+        <Content />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
