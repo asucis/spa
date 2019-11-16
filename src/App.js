6fde608c7e9b5b66ee395572dfe55d4856fb0d38
dickwyn <dickwyn@outlook.com>
2019-11-16 12:11:46 -0700

    (feat) remove international night popup

--- src/App.js
+++ src/App.js
@@ -1,55 +1,12 @@
-import React, { Component } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Hero from './components/Hero';
-import Content from './components/Content';
-import Footer from './components/Footer';
+import React, { Component } from "react";
+import Hero from "./components/Hero";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
 
 class App extends Component {
-  componentDidMount() {
-    toast.info(
-      <p>
-        <span role="img" aria-label="world-map">
-          🗺️
-        </span>{' '}
-        International Night 2019 is happening on November 15, Get your free
-        tickets <a href="https://signup.asucis.com">here</a>
-        <br />
-        <br />
-        Learn more about the event at{' '}
-        <a href="https://internationalnight.asucis.com">
-          internationalnight.asucis.com
-        </a>
-        <br />
-        <br />
-        #ASUINTLNIGHT
-        <br />
-        #asuglobal
-      </p>,
-      {
-        position: 'bottom-center',
-        autoClose: 30000,
-        hideProgressBar: true,
-        closeOnClick: false,
-        pauseOnHover: true,
-        draggable: true
-      }
-    );
-  }
   render() {
     return (
       <div className="App">
-        <ToastContainer
-          position="bottom-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnVisibilityChange
-          draggable
-          pauseOnHover
-        />
         <Hero />
         <Content />
         <Footer />
