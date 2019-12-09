d36ccd882896ee84ac639d57286d7f55b63d8978
dickwyn <dickwyn@outlook.com>
2019-12-08 22:17:57 -0700

    (refactor) styling and layout of error screen

--- src/components/Error.js
+++ src/components/Error.js
@@ -2,6 +2,15 @@ import React from "react";
 
 const Error = () => (
   <section className="error">
+    <h1>418 - I'm a teapot</h1>
+    <hr></hr>
+    <p>The requested entity body is short and stout.</p>
+    <p>Tip me over and pour me out.</p>
+    <br></br>
+    <p>
+      we're open source{" "}
+      <a href="https://github.com/asucis/">https://github.com/asucis/</a>
+    </p>
     <pre>
       {` 
  █████╗ ███████╗██╗   ██╗ ██████╗██╗███████╗
@@ -12,14 +21,6 @@ const Error = () => (
 ╚═╝  ╚═╝╚══════╝ ╚═════╝  ╚═════╝╚═╝╚══════╝
         `}
     </pre>
-    <h1>418 - I'm a teapot</h1>
-    <p>The requested entity body is short and stout.</p>
-    <p>Tip me over and pour me out.</p>
-    <br></br>
-    <p>
-      we're open source{" "}
-      <a href="https://github.com/asucis/">https://github.com/asucis/</a>
-    </p>
   </section>
 );
 
