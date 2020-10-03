2e2a3be1753656c0aeb770d3d8f6b1b561e3fffd
Dick Wyn Yong <dickwyn@outlook.com>
2020-10-02 21:27:09 -0700

    (chore) Reinstate app path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./scss/App.scss";
-// import App from "./App";
+import App from "./App";
 import Error from "./components/Error";
 import Wechat from "./screens/Wechat";
 import { Route, BrowserRouter as Router } from "react-router-dom";
@@ -9,8 +9,9 @@ import { Route, BrowserRouter as Router } from "react-router-dom";
 const router = (
   <Router>
     <div>
-      <Route exact path="/" component={Error} />
+      <Route exact path="/" component={App} />
       <Route path="/weixin" component={Wechat} />
+      <Route path='*' exact={true} component={Error} />
     </div>
   </Router>
 );
