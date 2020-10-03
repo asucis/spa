9ab578b9e25e4578fd1173c65562ad9032e077c1
Dick Wyn Yong <dickwyn@outlook.com>
2020-10-02 22:21:24 -0700

    (chore) Hide routing

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./scss/App.scss";
 import App from "./App";
-import Wechat from "./screens/Wechat";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+// import Wechat from "./screens/Wechat";
+// import { Route, HashRouter, Switch } from "react-router-dom";
 
-const router = (
-  <Router>
-    <div>
-      <Route exact path="/" component={App} />
-      <Route path="/weixin" component={Wechat} />
-    </div>
-  </Router>
-);
+// const router = (
+//   <HashRouter basename={process.env.PUBLIC_URL}>
+//     <Switch>
+//       <Route exact path={process.env.PUBLIC_URL + "/"} component={App} />
+//       <Route path={process.env.PUBLIC_URL + "/weixin"} component={Wechat} />
+//     </Switch>
+//   </HashRouter>
+// );
 
-ReactDOM.render(router, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
